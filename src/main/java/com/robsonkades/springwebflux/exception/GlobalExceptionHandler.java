5b3b9af3b0115e935364eef63428d2e15f29d7d5
Robson Kades <robsonkades@outlook.com>
2022-05-06 17:50:34 -0300

    [feat]: Global Exception Handler

---
+++ src/main/java/com/robsonkades/springwebflux/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.robsonkades.springwebflux.exception;
+
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Order(-2)
+@Component
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalExceptionHandler(ErrorAttributes errorAttributes, WebProperties webproperties,
+                                  ApplicationContext applicationContext, ServerCodecConfigurer configurer) {
+        super(errorAttributes, webproperties.getResources(), applicationContext);
+        this.setMessageWriters(configurer.getWriters());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::formatErrorResponse);
+    }
+
+    private Mono<ServerResponse> formatErrorResponse(ServerRequest request) {
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        int status = (int) Optional.ofNullable(errorAttributes.get("status")).orElse(500);
+
+        return ServerResponse
+                .status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorAttributes));
+    }
+}
