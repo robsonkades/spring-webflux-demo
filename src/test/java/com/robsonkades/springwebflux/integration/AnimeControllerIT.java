4a2f83b35692eda2a47da41c5a3b69cb446c47c8
Robson Kades <robsonkades@outlook.com>
2022-05-07 10:34:23 -0300

    [feat]: Webflux integration tests with WebTestClient

---
+++ src/test/java/com/robsonkades/springwebflux/integration/AnimeControllerIT.java
@@ -0,0 +1,98 @@
+package com.robsonkades.springwebflux.integration;
+
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.robsonkades.springwebflux.domain.Anime;
+import com.robsonkades.springwebflux.repository.AnimeRepository;
+import com.robsonkades.springwebflux.service.AnimeService;
+import com.robsonkades.springwebflux.util.AnimeCreator;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+@Import(AnimeService.class)
+public class AnimeControllerIT {
+
+    @MockBean
+    private AnimeRepository animeRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetup() {
+        BlockHound.install();
+    }
+
+    @Test
+    public void testBlockHound() {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+    @BeforeEach
+    public void each() {
+        BDDMockito
+                .when(animeRepository.findAll())
+                .thenReturn(Flux.just(anime));
+    }
+
+    @Test
+    @DisplayName("findAll returns a flux of anime")
+    public void findAll_ReturnFluxOfAnime_WhenSuccessful() {
+        webTestClient
+                .get()
+                .uri("/animes")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo(anime.getId())
+                .jsonPath("$.[0].name").isEqualTo(anime.getName());
+    }
+
+    @Test
+    @DisplayName("findAll returns a flux of anime")
+    public void findAll_Flavor2_ReturnFluxOfAnime_WhenSuccessful() {
+        webTestClient
+                .get()
+                .uri("/animes")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Anime.class)
+                .hasSize(1)
+                .contains(anime);
+    }
+}
